a3dfa20dced02a0d40aba1889e8c152241cea840
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2019-04-10 03:32:26 -0300

    chore: Move to next.js and now.sh

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import Layout from '../layouts/Main'
+
+const IndexPage: React.FunctionComponent = () => {
+  return (
+    <Layout title="Tandil.dev">
+      <h1>👋</h1>
+      <h2>We are Tandil dev</h2>
+      <p>Under construction. Please wait patiently</p>
+    </Layout>
+  )
+}
+
+export default IndexPage;
